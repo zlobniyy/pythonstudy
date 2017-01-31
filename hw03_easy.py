75f37e22740794d18f51e81023f7930b0e52293f
Кравченко Степан Борисович <skravchenko@win.ftc.ru>
2017-01-31 17:52:33 +0700

    -- hw4 - сделано easy в homework4

--- hw03_easy.py
+++ hw03_easy.py
@@ -1,36 +1,35 @@
+# coding: utf-8
+__author__ = "Кравченко Степан"
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом)
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math
-print ("="*20+"Задание-1"+"="*20)
+print( "=" * 20 + "Задание-1" + "=" * 20 )
 
 
 def my_round(number, ndigits):
     '''Это самопальная функция округления числа.'''
-    lsch=str(number).split(".")
-    ch1=lsch[0]
-    ch2=lsch[1]
+    lsch = str( number ).split( "." )
+    ch1 = lsch[0]
+    ch2 = lsch[1]
     num2 = list( map( int, str( ch2 ) ) )
     num1 = list( map( int, str( ch1 ) ) )
-    num2copy=num2
-    print( num1 )
-    print( num2 )
-    for i in range(len(num2copy)-1,ndigits,-1):
-        if num2[i] >4:
-           #num2[i-1] = num2[i-1]+1
-           num2[i - 1] += 1
-           num2[i] = 0
-        num2.pop(i)
-    print(num2)
+    num2copy = num2
+    for i in range( len( num2copy ) - 1, ndigits, -1 ):
+        if num2[i] > 4:
+            # num2[i-1] = num2[i-1]+1
+            num2[i - 1] += 1
+            num2[i] = 0
+        num2.pop( i )
+    # print(num2)
     if num2[0] > 4:
-        num1[0] +=1
-
-    print(("".join(str(num1))+"."+"".join(str(num2))))
-#my_round(0.23189498435, 5)
-my_round(2.1234567, 5)
+        num1[0] += 1
+    print( number )
+    print( "".join( map( str, num1 ) ) + "." + "".join( map( str, num2 ) ) )
 
 
+my_round( 2.1234567, 5 )
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -38,18 +37,19 @@ my_round(2.1234567, 5)
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
-print ("="*20+"Задание-2"+"="*20)
+print( "=" * 20 + "Задание-2" + "=" * 20 )
 
 
 def lucky_ticket(ticket_number):
     '''Функция определения счастливого билетика'''
 
-    part1 = list( map( int, str(ticket_number)[:8]))
-    part2 = list( map( int, str(ticket_number)[8:]))
-    if sum(part1) == sum(part2):
+    part1 = list( map( int, str( ticket_number )[:8] ) )
+    part2 = list( map( int, str( ticket_number )[8:] ) )
+    if sum( part1 ) == sum( part2 ):
         return 1
     else:
         return 0
 
-lucky_ticket(1234567890123456)
-lucky_ticket(1212121212121212)
+
+lucky_ticket( 1234567890123456 )
+lucky_ticket( 1212121212121212 )
---
+++ hw04_normal.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+__author__="Кравченко Степан"
+import re
+# Задание-1:
+# Вывести символы в нижнем регистре, которые окружают 1 или более символа в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+print ("="*20+"Задание-1"+"="*20)
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
+       'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
+       'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
+       'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
+       'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
+       'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
+       'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
+       'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
+       'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
+       'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
+       'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
+       'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+string11=re.findall('[a-z]+[A-Z]+',line)
+print(string11)
+
+
+# Задание-2:
+# Вывести символы в верхнем регистре, которые окружают ровно два символа в нижнем регистре слева
+# и два символа в верхнем регистре справа. Решить задачу двумя способами: с помощью re и без.
+# Т.е. из строки "sGAMkgAYEOmHBSQs" нужно получить ['GAM', 'EO']
+print ("="*20+"Задание-2"+"="*20)
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp' \
+       'PLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANv' \
+       'IoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkp' \
+       'YOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSA' \
+       'fJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAb' \
+       'fCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyA' \
+       'xDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFN' \
+       'IsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUf' \
+       'lwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZC' \
+       'nZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxy' \
+       'GPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXi' \
+       'UWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+
+string21 = re.findall('[A-Z]+[a-z]+',line_2)
+print(string21)
+string22=map(lambda )
+# Задача-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла) произвольными целыми
+# числами, в результате в файле должно быть 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр в вышезаполненном файле.
+print ("="*20+"Задание-3"+"="*20)
